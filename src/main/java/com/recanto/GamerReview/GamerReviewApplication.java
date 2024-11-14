31af5575bc60c9c087c0afa329d56ca1c5260821
LeonardoSlonzo <leonardoslonzo@hotmail.com>
2024-11-14 17:25:31 -0300

    Primeiro

---
+++ src/main/java/com/recanto/GamerReview/GamerReviewApplication.java
@@ -0,0 +1,13 @@
+package com.recanto.GamerReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recanto/GamerReview/GamerReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recanto.GamerReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
